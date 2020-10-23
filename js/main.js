22180dc1f82ad37d98514a9f8ff0de4609f8e6a1
sebarabash <62248308+sebarabash@users.noreply.github.com>
2020-10-23 23:29:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+$(document).ready(function () {
+  $(".sidebar__burger-wrapper").click(function () {
+    $(".sidebar__name").toggleClass("active");
+    $(".content__sidebar").toggleClass("active");
+    $(".sidebar__link").toggleClass("active");
+    $(".sidebar__burger").toggleClass("burger__active");
+  });
+});
+// ////////////////////// slider //////////////////////////////
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
